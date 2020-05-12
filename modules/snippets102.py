74cccfeb1f230c42ea8a0f4c32f4c46311bbb99d
pheidelb <50695150+pheidelb@users.noreply.github.com>
2020-05-12 09:50:15 +0200

    Update Comments and DocString in snippets102.py

--- modules/snippets102.py
+++ modules/snippets102.py
@@ -1,15 +1,5 @@
-# Snippets.py
-# ************************************************
-# Updated for ArcGIS 10.2
-# ************************************************
-# Requires installation of the comtypes package
-# Available at: http://sourceforge.net/projects/comtypes/
-# Once comtypes is installed, the following modifications
-# need to be made for compatibility with ArcGIS 10.2:
-# 1) Delete automation.pyc, automation.pyo, safearray.pyc, safearray.pyo
-# 2) Edit automation.py
-# 3) Add the following entry to the _ctype_to_vartype dictionary (line 794):
-#    POINTER(BSTR): VT_BYREF|VT_BSTR,
+# get_lib_path() and type_cast_module() from 
+# http://pierssen.com/arcgis10/python.htm
 # ************************************************
 
 
@@ -17,7 +7,7 @@ def unpack2rgb(rgb_long):
     """ This function converts a RGB-Long value to a Standard RGB-Code
 
     :param rgb_long: the RGB-Value as long
-    :return:
+    :return: Standard RGB-Values as String 
     """
     red = rgb_long & 255
     green = (rgb_long >> 8) & 255
@@ -26,7 +16,7 @@ def unpack2rgb(rgb_long):
     try:
         return result
     except (ValueError, Exception):
-        return "255", "255", "255", "255"
+        return "255, 255, 255, 255"
 
 
 def get_lib_path():
