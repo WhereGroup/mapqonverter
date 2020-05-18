8a91cb82ada82885ad6ab9e6653de0a0f0d6328c
Peter Heidelbach <peter.heidelbach@wheregroup.com>
2020-05-18 17:08:54 +0200

    Adapt Variables in the header

--- map/header.py
+++ map/header.py
@@ -1,34 +1,28 @@
 # coding=utf-8
-def create_header(doc):
+def create_header(dom):
     """ Creates the Header in the DOM
 
-    :param doc: The DOM / Document for writing the QGIS-File
-    :return: the qgis element in the DOM
+    :param dom: The DOM / Document for writing the QGIS-File
+    :return: the header element in the DOM
     """
-    # Create the <qgis> base element
-    qgis = doc.createElement("qgis")
-    qgis.setAttribute("projectname", "")
-    qgis.setAttribute("version", unicode("3.10.0-A Coruña", 'UTF-8'))
-    doc.appendChild(qgis)
+    header_element = dom.createElement("qgis")
+    header_element.setAttribute("projectname", "")
+    header_element.setAttribute("version", unicode("3.10.0-A Coruña", 'UTF-8'))
+    dom.appendChild(header_element)
 
-    # Create the <homepath> element
-    home_path = doc.createElement("homePath")
+    home_path_element = dom.createElement("homePath")
+    home_path_element.setAttribute("path", "")
+    header_element.appendChild(home_path_element)
 
-    home_path.setAttribute("path", "")
-    qgis.appendChild(home_path)
+    title_element = dom.createElement("title")
+    header_element.appendChild(title_element)
 
-    # Create the <title> element
-    title = doc.createElement("title")
-    qgis.appendChild(title)
+    autotransaction_element = dom.createElement("autotransaction")
+    autotransaction_element.setAttribute("active", "0")
+    header_element.appendChild(autotransaction_element)
 
-    # Create the <autostransaction> element
-    autotransaction = doc.createElement("autotransaction")
-    autotransaction.setAttribute("active", "0")
-    qgis.appendChild(autotransaction)
+    evaluate_default_values_element = dom.createElement("evaluateDefaultValues")
+    evaluate_default_values_element.setAttribute("active", "0")
+    header_element.appendChild(evaluate_default_values_element)
 
-    # Create the <evaluateDefaultValues> element
-    evaluate_default_values = doc.createElement("evaluateDefaultValues")
-    evaluate_default_values.setAttribute("active", "0")
-    qgis.appendChild(evaluate_default_values)
-
-    return qgis
+    return header_element
