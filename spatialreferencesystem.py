b3da7d424439aacd208980f86b929e842b86d2c0
Peter Heidelbach <peter.heidelbach@wheregroup.com>
2020-05-18 17:13:09 +0200

    Change Variables in spatialreferencesystem

--- spatialreferencesystem.py
+++ spatialreferencesystem.py
@@ -9,57 +9,53 @@ class SpatialReferenceSystem:
         self.projectionacronym = projectionacronym
         self.geographicflag = geographicflag
 
-    def create_spatialrefsys(self, doc, project_crs):
+    def create_spatialrefsys(self, dom, project_crs):
         """ Creates the SRS_element for the DOM
 
-        :param doc: The DOM / Document for writing the QGIS-File
+        :param dom: The DOM / Document for writing the QGIS-File
         :param project_crs: the project_crs_element in the DOM
         :return: the spatial_ref_sys_element
         """
-        spatial_ref_sys_element = doc.createElement("spatialrefsys")
+        spatial_ref_sys_element = dom.createElement("spatialrefsys")
         project_crs.appendChild(spatial_ref_sys_element)
 
-        proj4_element = doc.createElement("proj4")
-        proj4_content = doc.createTextNode(self.proj4)
+        proj4_element = dom.createElement("proj4")
+        proj4_content = dom.createTextNode(self.proj4)
         proj4_element.appendChild(proj4_content)
         spatial_ref_sys_element.appendChild(proj4_element)
 
-        srsid_element = doc.createElement("srsid")
-        srsid_content = doc.createTextNode(self.srsid)
+        srsid_element = dom.createElement("srsid")
+        srsid_content = dom.createTextNode(self.srsid)
         srsid_element.appendChild(srsid_content)
         spatial_ref_sys_element.appendChild(srsid_element)
 
-        srid_element = doc.createElement("srid")
-        srid_content = doc.createTextNode(self.srid)
+        srid_element = dom.createElement("srid")
+        srid_content = dom.createTextNode(self.srid)
         srid_element.appendChild(srid_content)
         spatial_ref_sys_element.appendChild(srid_element)
 
-        authid_element = doc.createElement("authid")
-        authid_content = doc.createTextNode(self.authid)
+        authid_element = dom.createElement("authid")
+        authid_content = dom.createTextNode(self.authid)
         authid_element.appendChild(authid_content)
         spatial_ref_sys_element.appendChild(authid_element)
 
-        # Create the <description> element
-        description_element = doc.createElement("description")
-        description_content = doc.createTextNode(self.description)
+        description_element = dom.createElement("description")
+        description_content = dom.createTextNode(self.description)
         description_element.appendChild(description_content)
         spatial_ref_sys_element.appendChild(description_element)
 
-        # Create the <projectionacronym> element
-        projectionacronym = doc.createElement("projectionacronym")
-        content = doc.createTextNode(self.projectionacronym)
+        projectionacronym = dom.createElement("projectionacronym")
+        content = dom.createTextNode(self.projectionacronym)
         projectionacronym.appendChild(content)
         spatial_ref_sys_element.appendChild(projectionacronym)
 
-        # Create the <ellipsoidacronym element
-        ellipsoidacronym_element = doc.createElement("ellipsoidacronym")
-        ellipsoidacronym_content = doc.createTextNode(self.ellipsoidacronym)
+        ellipsoidacronym_element = dom.createElement("ellipsoidacronym")
+        ellipsoidacronym_content = dom.createTextNode(self.ellipsoidacronym)
         ellipsoidacronym_element.appendChild(ellipsoidacronym_content)
         spatial_ref_sys_element.appendChild(ellipsoidacronym_element)
 
-        # Create the <geographicflag> element
-        geographicflag_element = doc.createElement("geographicflag")
-        geographicflag_content = doc.createTextNode(self.geographicflag)
+        geographicflag_element = dom.createElement("geographicflag")
+        geographicflag_content = dom.createTextNode(self.geographicflag)
         geographicflag_element.appendChild(geographicflag_content)
         spatial_ref_sys_element.appendChild(geographicflag_element)
 
