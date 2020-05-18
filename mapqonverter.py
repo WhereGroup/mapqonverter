c7081c6e186da0244cd821388858979939b89e77
Peter Heidelbach <peter.heidelbach@wheregroup.com>
2020-05-18 17:32:19 +0200

    Change document-variable to xml_document in mainFile/srs

--- mapqonverter.py
+++ mapqonverter.py
@@ -74,9 +74,9 @@ def main():
     qgs_file_name = "{}.qgs".format(export_name_short)
     qgs_file = codecs.open(qgs_file_name, "w", encoding="utf-8")
 
-    dom = Document()
+    xml_document = Document()
 
-    header = create_header(dom)
+    header = create_header(xml_document)
 
     arcpy.AddMessage("Scanning Project and collecting Layer-Information")
 
@@ -103,24 +103,24 @@ def main():
     for counter, dataframe in enumerate(arcpy.mapping.ListDataFrames(mxd)):
         if counter == 0:
             print 'Creating MapSpatialReferenceSystem........'
-            MapSpatialReferenceSystem.create_map_srs_element(dom, header, dataframe)
+            MapSpatialReferenceSystem.create_map_srs_element(xml_document, header, dataframe)
 
         arc_dataframe = arc_doc.Maps.Item[counter]
         layer_list = arcpy.mapping.ListLayers(dataframe)
 
         arcpy.AddMessage("{} Layers are in the Dataframe".format(str(len(layer_list))))
 
-        ProjectLayers.create_project_layers_element(dom, header, layer_list, arc_dataframe)
+        ProjectLayers.create_project_layers_element(xml_document, header, layer_list, arc_dataframe)
         broken_layer_list = brokenLayers.BrokenLayers.broken_layer_list
         for broken_layer in broken_layer_list:
             if broken_layer in layer_list:
                 layer_list.remove(broken_layer)
-        MapLegend.create_map_legend_element(dom, header, layer_list, dataframe)
-        LayerTree.create_layertree(dom, header, layer_list, dataframe)
-        MapProperties.create_map_properties_element(dom, header)
+        MapLegend.create_map_legend_element(xml_document, header, layer_list, dataframe)
+        LayerTree.create_layertree(xml_document, header, layer_list, dataframe)
+        MapProperties.create_map_properties_element(xml_document, header)
 
     try:
-        dom.writexml(qgs_file, indent='  ', addindent='  ', newl='\n', encoding='UTF-8')
+        xml_document.writexml(qgs_file, indent='  ', addindent='  ', newl='\n', encoding='UTF-8')
         arcpy.AddMessage("Project saved!")
         arcpy.AddMessage('QGIS-File written')
     finally:
--- spatialreferencesystem.py
+++ spatialreferencesystem.py
@@ -9,53 +9,53 @@ class SpatialReferenceSystem:
         self.projectionacronym = projectionacronym
         self.geographicflag = geographicflag
 
-    def create_spatialrefsys(self, dom, project_crs):
+    def create_spatialrefsys(self, xml_document, project_crs):
         """ Creates the SRS_element for the DOM
 
-        :param dom: The DOM / Document for writing the QGIS-File
+        :param xml_document: The DOM / Document for writing the QGIS-File
         :param project_crs: the project_crs_element in the DOM
         :return: the spatial_ref_sys_element
         """
-        spatial_ref_sys_element = dom.createElement("spatialrefsys")
+        spatial_ref_sys_element = xml_document.createElement("spatialrefsys")
         project_crs.appendChild(spatial_ref_sys_element)
 
-        proj4_element = dom.createElement("proj4")
-        proj4_content = dom.createTextNode(self.proj4)
+        proj4_element = xml_document.createElement("proj4")
+        proj4_content = xml_document.createTextNode(self.proj4)
         proj4_element.appendChild(proj4_content)
         spatial_ref_sys_element.appendChild(proj4_element)
 
-        srsid_element = dom.createElement("srsid")
-        srsid_content = dom.createTextNode(self.srsid)
+        srsid_element = xml_document.createElement("srsid")
+        srsid_content = xml_document.createTextNode(self.srsid)
         srsid_element.appendChild(srsid_content)
         spatial_ref_sys_element.appendChild(srsid_element)
 
-        srid_element = dom.createElement("srid")
-        srid_content = dom.createTextNode(self.srid)
+        srid_element = xml_document.createElement("srid")
+        srid_content = xml_document.createTextNode(self.srid)
         srid_element.appendChild(srid_content)
         spatial_ref_sys_element.appendChild(srid_element)
 
-        authid_element = dom.createElement("authid")
-        authid_content = dom.createTextNode(self.authid)
+        authid_element = xml_document.createElement("authid")
+        authid_content = xml_document.createTextNode(self.authid)
         authid_element.appendChild(authid_content)
         spatial_ref_sys_element.appendChild(authid_element)
 
-        description_element = dom.createElement("description")
-        description_content = dom.createTextNode(self.description)
+        description_element = xml_document.createElement("description")
+        description_content = xml_document.createTextNode(self.description)
         description_element.appendChild(description_content)
         spatial_ref_sys_element.appendChild(description_element)
 
-        projectionacronym = dom.createElement("projectionacronym")
-        content = dom.createTextNode(self.projectionacronym)
+        projectionacronym = xml_document.createElement("projectionacronym")
+        content = xml_document.createTextNode(self.projectionacronym)
         projectionacronym.appendChild(content)
         spatial_ref_sys_element.appendChild(projectionacronym)
 
-        ellipsoidacronym_element = dom.createElement("ellipsoidacronym")
-        ellipsoidacronym_content = dom.createTextNode(self.ellipsoidacronym)
+        ellipsoidacronym_element = xml_document.createElement("ellipsoidacronym")
+        ellipsoidacronym_content = xml_document.createTextNode(self.ellipsoidacronym)
         ellipsoidacronym_element.appendChild(ellipsoidacronym_content)
         spatial_ref_sys_element.appendChild(ellipsoidacronym_element)
 
-        geographicflag_element = dom.createElement("geographicflag")
-        geographicflag_content = dom.createTextNode(self.geographicflag)
+        geographicflag_element = xml_document.createElement("geographicflag")
+        geographicflag_content = xml_document.createTextNode(self.geographicflag)
         geographicflag_element.appendChild(geographicflag_content)
         spatial_ref_sys_element.appendChild(geographicflag_element)
 
