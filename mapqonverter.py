80abadf661bb0de67adfc387efc4604f1dab8243
Peter Heidelbach <peter.heidelbach@wheregroup.com>
2020-05-19 16:28:19 +0200

    Rewrite XML-Formatting

--- mapqonverter.py
+++ mapqonverter.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python2.7
 # encoding: utf-8
 import codecs
-import xml
 from xml.dom.minidom import Document
 from zipfile import ZipFile
 import os
@@ -75,7 +74,7 @@ def main():
         MapProperties.create_map_properties_element(xml_document, header)
 
     try:
-        xml_document.writexml(qgs_file, indent='  ', addindent='  ', newl='\n', encoding='UTF-8')
+        qgs_file.write(xml_document.toprettyxml(indent="    "))
         arcpy.AddMessage("Project saved!")
         arcpy.AddMessage('QGIS-File written')
     finally:
@@ -96,42 +95,6 @@ def main():
             os.remove(qgd_file_name)
         except OSError as e:
             print ("Error: %s - %s." % (e.filename, e.strerror))
-
-
-# http://www.ronrothman.com/public/leftbraned/xml-dom-minidom-toprettyxml-and-silly-whitespace/
-def fixed_writexml(self, writer, indent="", addindent="", newl=""):
-    """ This functions has fixed formatting for the created XML-File
-    :param writer = the file to write to
-    :param indent = current indentation
-    :param addindent = indentation to add to higher levels
-    :param newl = newline string
-    """
-    writer.write(indent+"<" + self.tagName)
-
-    attrs = self._get_attributes()
-    a_names = attrs.keys()
-    a_names.sort()
-
-    for a_name in a_names:
-        writer.write(" %s=\"" % a_name)
-        xml.dom.minidom._write_data(writer, attrs[a_name].value)
-        writer.write("\"")
-    if self.childNodes:
-        if len(self.childNodes) == 1 \
-          and self.childNodes[0].nodeType == xml.dom.minidom.Node.TEXT_NODE:
-            writer.write(">")
-            self.childNodes[0].writexml(writer, "", "", "")
-            writer.write("</%s>%s" % (self.tagName, newl))
-            return
-        writer.write(">%s" % newl)
-        for node in self.childNodes:
-            node.writexml(writer, indent+addindent, addindent, newl)
-        writer.write("%s</%s>%s" % (indent, self.tagName, newl))
-    else:
-        writer.write("/>%s" % newl)
-
-
-xml.dom.minidom.Element.writexml = fixed_writexml
 
 
 if __name__ == "__main__":
