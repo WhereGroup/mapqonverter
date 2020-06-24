a90b5a280e6bc3c985901dd5e5edde4560803a3b
Peter Heidelbach <peter.heidelbach@wheregroup.com>
2020-06-24 13:00:00 +0200

    Create Dictionaries for Layout

---
+++ dictionaries/layoutItemsDict.py
@@ -0,0 +1,54 @@
+dict_arcpy_arcObj_types = {
+    "Text": "TEXT_ELEMENT",
+    "Data Frame": "DATAFRAME_ELEMENT",
+    "Map Surround Frame": "MAPSURROUND_ELEMENT",
+    "Legend": "LEGEND_ELEMENT",
+    "North Arrow": "MAPSURROUND_ELEMENT",
+    "Scale Bar": "MAPSURROUND_ELEMENT",
+    "Rectangle": "GRAPHIC_ELEMENT",
+    'Ellipse': 'GRAPHIC_ELEMENT',
+    'Circle': 'GRAPHIC_ELEMENT',
+    'Polygon': 'GRAPHIC_ELEMENT',
+    "Line":  "GRAPHIC_ELEMENT",
+    "Picture": "PICTURE_ELEMENT",
+}
+
+
+dict_units = {
+    0 	: "Unknown",
+    1 	: "In",
+    2 	: "Pt",
+    3 	: "Ft",
+    4 	: "Yd",
+    5 	: "MI",
+    6 	: "SM",
+    7 	: "MM",
+    8 	: "CM",
+    9 	: "M",
+    10 	: "KM",
+    11 	: "DD",
+    12 	: "DM",
+}
+
+dict_vertical_position = {
+    0: '0',
+    1: '0',
+    2: '0',
+    3: '1',
+    4: '1',
+}
+
+dict_line_style = {
+    0: 'Line Ticks Up',
+    1: 'Line Ticks Up',
+    2: 'Line Ticks Middle',
+    3: 'Line Ticks Down',
+    4: 'Line Ticks Down',
+}
+
+dict_geometry = {
+    'Rectangle': '1',
+    'Ellipse': '0',
+    'Circle': '0',
+    'Polygon': '2',
+}
