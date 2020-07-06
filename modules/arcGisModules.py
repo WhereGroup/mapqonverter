d693c20bc6dac8d2c4d81903f3e5118ac50575ff
pheidelb <50695150+pheidelb@users.noreply.github.com>
2020-07-06 11:13:27 +0200

    Update arcGisModules.py

--- modules/arcGisModules.py
+++ modules/arcGisModules.py
@@ -8,10 +8,10 @@ class ArcGisModules:
         pass
 
     lib_path = get_lib_path()
-    module_carto = GetModule(lib_path + "esriCarto.olb")
-    module_display = GetModule(lib_path + "esriDisplay.olb")
-    module_gdb = GetModule(lib_path + "esriGeoDatabase.olb")
-    module_data_source_raster = GetModule(lib_path + "esriDataSourcesRaster.olb")
-    module_framework = GetModule(lib_path + "esriFramework.olb")
-    module_map_ui = GetModule(lib_path + "esriArcMapUI.olb")
+    module_carto = GetModule("{lib_path}esriCarto.olb".format(lib_path=lib_path))
+    module_display = GetModule("{lib_path}esriDisplay.olb".format(lib_path=lib_path))
+    module_gdb = GetModule("{lib_path}esriGeoDatabase.olb".format(lib_path=lib_path))
+    module_data_source_raster = GetModule("{lib_path}esriDataSourcesRaster.olb".format(lib_path=lib_path))
+    module_framework = GetModule("{lib_path}esriFramework.olb".format(lib_path=lib_path))
+    module_map_ui = GetModule("{lib_path}esriArcMapUI.olb".format(lib_path=lib_path))
     module_geometry = GetModule("{lib_path}esriGeometry.olb".format(lib_path=lib_path))
