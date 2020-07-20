e294f5d42e3652fd6e6f0c4740d8887de0525c72
Peter Heidelbach <peter.heidelbach@wheregroup.com>
2020-07-20 13:57:46 +0200

    Set Grouping to longName in Layertree and create functions for creating Elements

--- map/layerTree.py
+++ map/layerTree.py
@@ -35,54 +35,65 @@ class LayerTree:
         for index, layer in enumerate(layerlist):
             # 2 (or more) grouplayer cant have the same name, if they are in the same branch and direct neighbours, sad
             if layer.isGroupLayer:
-                layer_tree_group[layer.name] = dom.createElement("layer-tree-group")
-                layer_tree_group[layer.name].setAttribute("name", layer.name)
-                if layer.visible:
-                    layer_tree_group[layer.name].setAttribute("checked", "Qt::Checked")
-                else:
-                    layer_tree_group[layer.name].setAttribute("checked", "Qt::Unchecked")
-                layer_tree_group[layer.name].setAttribute("expanded", "1")
-                # check long name for forgoing layers, append if exists, otherwise add to layer_tree_
-                try:
-                    layer_tree_group[layer.longName.split("\\")[-2]].appendChild(layer_tree_group[layer.name])
-                except IndexError:
-                    dataframe_to_group_layer_element.appendChild(layer_tree_group[layer.name])
-                custom_properties_element = dom.createElement("customproperties")
-                layer_tree_group[layer.name].appendChild(custom_properties_element)
+                LayerTree.create_group_layer_element(layer_tree_group, layer, dataframe_to_group_layer_element, dom)
             else:
-                layer_tree_layer = dom.createElement("layer-tree-layer")
-
-                if layer.supports('DATASOURCE'):
-                    layer_tree_element_source = layer.dataSource
-                    if ".gdb" in layer_tree_element_source:
-                        layer_tree_element_source = '|layername='.join(layer_tree_element_source.rsplit('\\', 1))
-                    layer_tree_layer.setAttribute("source", layer_tree_element_source)
-
-                if layer.visible:
-                    layer_tree_layer.setAttribute("checked", "Qt::Checked")
-                else:
-                    layer_tree_layer.setAttribute("checked", "Qt::Unchecked")
-
-                layer_tree_layer.setAttribute("name", layer.name)
-
-                layer_tree_layer.setAttribute("id",  u"{name}20190727170816078".format(name=layer.longName))
-
-                layer_tree_layer.setAttribute("expanded", "0")
-
-                maplayer_elements = dom.getElementsByTagName('maplayer')[map_layer_count]
-                provider_value = maplayer_elements.getElementsByTagName('provider')[0].firstChild.nodeValue
-                layer_tree_layer.setAttribute("providerKey", provider_value)
-                map_layer_count += 1
-
-                # same as for grouplayer-elements
-                try:
-                    layer_tree_group[layer.longName.split("\\")[-2]].appendChild(layer_tree_layer)
-                except IndexError:
-                    dataframe_to_group_layer_element.appendChild(layer_tree_layer)
-
-                custom_properties_element = dom.createElement("customproperties")
-                layer_tree_layer.appendChild(custom_properties_element)
+                LayerTree.create_layer_element(layer_tree_group, layer, dataframe_to_group_layer_element, dom, map_layer_count)
 
         custom_order_element = dom.createElement("custom-order")
         custom_order_element.setAttribute("enabled", "0")
         layer_tree_element.appendChild(custom_order_element)
+
+    @staticmethod
+    def create_group_layer_element(layer_tree_group, layer, dataframe_to_group_layer_element, dom):
+        layer_tree_group[layer.longName] = dom.createElement("layer-tree-group")
+        layer_tree_group[layer.longName].setAttribute("name", layer.name)
+        if layer.visible:
+            layer_tree_group[layer.longName].setAttribute("checked", "Qt::Checked")
+        else:
+            layer_tree_group[layer.longName].setAttribute("checked", "Qt::Unchecked")
+        layer_tree_group[layer.longName].setAttribute("expanded", "1")
+        # check long name for forgoing layers, append if exists, otherwise add to layer_tree_
+        forgoing_layer = layer.longName.rsplit("\\", 1)[0]
+        if forgoing_layer == layer.longName:
+            dataframe_to_group_layer_element.appendChild(layer_tree_group[layer.longName])
+        else:
+            layer_tree_group[forgoing_layer].appendChild(layer_tree_group[layer.longName])
+
+        custom_properties_element = dom.createElement("customproperties")
+        layer_tree_group[layer.longName].appendChild(custom_properties_element)
+
+    @staticmethod
+    def create_layer_element(layer_tree_group, layer, dataframe_to_group_layer_element, dom, map_layer_count):
+        layer_tree_layer = dom.createElement("layer-tree-layer")
+
+        if layer.supports('DATASOURCE'):
+            layer_tree_element_source = layer.dataSource
+            if ".gdb" in layer_tree_element_source:
+                layer_tree_element_source = '|layername='.join(layer_tree_element_source.rsplit('\\', 1))
+            layer_tree_layer.setAttribute("source", layer_tree_element_source)
+
+        if layer.visible:
+            layer_tree_layer.setAttribute("checked", "Qt::Checked")
+        else:
+            layer_tree_layer.setAttribute("checked", "Qt::Unchecked")
+
+        layer_tree_layer.setAttribute("name", layer.name)
+
+        layer_tree_layer.setAttribute("id", u"{name}20190727170816078".format(name=layer.longName))
+
+        layer_tree_layer.setAttribute("expanded", "0")
+
+        maplayer_elements = dom.getElementsByTagName('maplayer')[map_layer_count]
+        provider_value = maplayer_elements.getElementsByTagName('provider')[0].firstChild.nodeValue
+        layer_tree_layer.setAttribute("providerKey", provider_value)
+        map_layer_count += 1
+
+        # same as for grouplayer-elements
+        forgoing_layer = layer.longName.rsplit("\\", 1)[0]
+        if forgoing_layer == layer.longName:
+            dataframe_to_group_layer_element.appendChild(layer_tree_layer)
+        else:
+            layer_tree_group[forgoing_layer].appendChild(layer_tree_layer)
+
+        custom_properties_element = dom.createElement("customproperties")
+        layer_tree_layer.appendChild(custom_properties_element)
