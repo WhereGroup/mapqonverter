acca075ccdba1a5444b74047b813aba18ffcc6be
Peter Heidelbach <peter.heidelbach@wheregroup.com>
2021-07-02 14:57:47 +0200

    Add german naming to dict

--- dictionaries/layoutItemsDict.py
+++ dictionaries/layoutItemsDict.py
@@ -2,15 +2,20 @@ dict_arcpy_arcObj_types = {
     "Text": "TEXT_ELEMENT",
     "Data Frame": "DATAFRAME_ELEMENT",
     "Map Surround Frame": "MAPSURROUND_ELEMENT",
+    "Kartenumgebungsrahmen": "MAPSURROUND_ELEMENT",
     "Legend": "LEGEND_ELEMENT",
     "North Arrow": "MAPSURROUND_ELEMENT",
     "Scale Bar": "MAPSURROUND_ELEMENT",
     "Rectangle": "GRAPHIC_ELEMENT",
+    "Rechteck": "GRAPHIC_ELEMENT",
     'Ellipse': 'GRAPHIC_ELEMENT',
     'Circle': 'GRAPHIC_ELEMENT',
+    'Kreis': 'GRAPHIC_ELEMENT',
     'Polygon': 'GRAPHIC_ELEMENT',
     "Line":  "GRAPHIC_ELEMENT",
+    "Linie":  "GRAPHIC_ELEMENT",
     "Picture": "PICTURE_ELEMENT",
+    "Bild": "PICTURE_ELEMENT",
 }
 
 
@@ -48,7 +53,9 @@ dict_line_style = {
 
 dict_geometry = {
     'Rectangle': '1',
+    'Rechteck': '1',
     'Ellipse': '0',
     'Circle': '0',
+    'Kreis': '0',
     'Polygon': '2',
 }
